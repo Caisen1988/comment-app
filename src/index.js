d9715d353521ef9fd1f18a84d0d3eb00ca1518e4
caisen <835596913@qq.com>
2018-03-21 14:22:31 +0800

    finish 练习题

--- src/index.js
+++ src/index.js
@@ -7,12 +7,77 @@
 // ReactDOM.render(<App />, document.getElementById('root'));
 // registerServiceWorker();
 
-import React from 'react'
+import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 import CommentApp from './CommentApp'
 import './index.css'
 
+/*
+测试题：百分比转换器
+*/
+
+class Input extends Component {
+    constructor() {
+        super()
+        this.state = {
+            number: 0
+        }
+    }
+
+    inputClickHandle(event) {
+        this.setState({number: event.target.value},
+            () => {
+                if (this.props.onSubmit) {
+                    const {number} = this.state
+                    this.props.onSubmit({number})
+                }
+            }
+        )
+    }
+
+    render() {
+        return (
+            <div>
+                <input type='number' onChange={this.inputClickHandle.bind(this)}/>
+            </div>
+        )
+    }
+}
+
+class PercentageShower extends Component {
+    render() {
+        return (
+            <div>
+                {Math.round(this.props.number * 100) / 100 * 100}%
+            </div>
+        )
+    }
+}
+
+class PercentageApp extends Component {
+    constructor() {
+        super()
+        this.state = {
+            number: 0
+        }
+    }
+
+    parmHandle(number) {
+        this.setState(number)
+    }
+
+    render() {
+        return (
+            <div>
+                <Input onSubmit={this.parmHandle.bind(this)}/>
+                <PercentageShower number={this.state.number}/>
+            </div>
+        )
+    }
+}
+
+
 ReactDOM.render(
-    <CommentApp/>,
+    <PercentageApp/>,
     document.getElementById('root')
 );
